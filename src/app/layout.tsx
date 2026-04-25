7f8c3091270c481d831fab7878c6c4a1f314988e
Muhammad abdullah <abdullah032518@gamil.com>
2026-04-25 12:41:27 +0500

    pixl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,11 +39,29 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Organocity" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#C6A24A" />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1520641896287637');fbq('track','PageView');`,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         <Providers>
           {children}
         </Providers>
+        {/* Meta Pixel Code - Noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1520641896287637&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <InstallPrompt />
         <ServiceWorkerRegistration />
       </body>
